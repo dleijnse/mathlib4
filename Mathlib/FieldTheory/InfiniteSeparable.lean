91ed737bab0e3561c028b4ac662ce12ad3656002
ARand0mCoder <d.leijnse@gmail.com>
2025-11-04 13:17:48 +0100

    working on proving small things

--- Mathlib/FieldTheory/InfiniteSeparable.lean
+++ Mathlib/FieldTheory/InfiniteSeparable.lean
@@ -6,13 +6,79 @@ Authors: Dion Leijnse
 
 import Mathlib
 
-def separablyGeneratedBy (k K : Type) [Field k] [Field K] [Algebra k K] (X : Set K) :=
-  IsTranscendenceBasis k (fun x => x.val : X → K)
-    ∧ Algebra.IsSeparable (IntermediateField.adjoin k X) K
+def separablyGeneratedBy (k K : Type) [Field k] [Field K] [Algebra k K] {ι : Type} (x : ι → K) :=
+  IsTranscendenceBasis k x ∧ Algebra.IsSeparable (IntermediateField.adjoin k (Set.range x)) K
+
+lemma AlgebraicExtensionEmptyTranscendenceBasis (k K : Type) [Field k] [Field K] [Algebra k K]
+    [Algebra.IsAlgebraic k K] : IsTranscendenceBasis k (@Empty.elim K) := by
+  obtain ⟨ι, y, hy⟩ := exists_isTranscendenceBasis' k K
+  have hιEmpty := (IsTranscendenceBasis.isEmpty_iff_isAlgebraic hy).mpr
+    Algebra.IsIntegral.isAlgebraic
+  have fι := (Equiv.equivEmpty ι).symm
+  have hcomp : Empty.elim = y ∘ ⇑fι := by
+    ext x
+    tauto
+  exact hcomp ▸ @IsTranscendenceBasis.comp_equiv Empty ι k K _ _ _ fι y hy
 
 lemma Separable_imp_separablyGeneratedByEmpty (k K : Type) [Field k] [Field K] [Algebra k K]
-    [Algebra.IsSeparable k K] : separablyGeneratedBy k K ∅ := by
-  constructor
+    [Algebra.IsSeparable k K] : separablyGeneratedBy k K (@Empty.elim K) := by
+  refine ⟨AlgebraicExtensionEmptyTranscendenceBasis _ _, ?_⟩
+  infer_instance
+
+def InfiniteSeparable (k K : Type) [Field k] [Field K] [Algebra k K] :=
+  ∀ K' : IntermediateField k K, Algebra.EssFiniteType k K' →
+    ∃ ι : Type, ∃ x : ι → K, separablyGeneratedBy K' K x
+
+def adjoin_equiv (R S T : Type) [CommRing R] [CommRing S] [CommRing T] [Algebra R S] [Algebra R T]
+    (f : S ≃ₐ[R] T) (X : Set S) : Algebra.adjoin R X ≃ₐ[R] Algebra.adjoin R (f '' X) :=
+  (AlgHom.map_adjoin f.toAlgHom X) ▸ (AlgEquiv.subalgebraMap f (Algebra.adjoin R X))
+
+example (R S T : Type) [CommRing R] [CommRing S] [CommRing T] [Algebra R S] [Algebra R T]
+    (f : S →ₐ[R] T) (X : Set S) (s : S) (h : s ∈ Algebra.adjoin R X) :
+  f s ∈ Algebra.adjoin R (f '' X) := by
+  rw [Algebra.adjoin_image, Subalgebra.mem_map]
+  use s
+
+example (R S T : Type) [CommRing R] [CommRing S] [CommRing T] [Algebra R S] [Algebra R T]
+    (f : S ≃ₐ[R] T) (h : Algebra.EssFiniteType R S) : Algebra.EssFiniteType R T := by
+  /-obtain ⟨s, hs⟩ := h
+  use (Finset.map f s)
+  let fs : (Algebra.adjoin R s.toSet) ≃ₐ[R] (Algebra.adjoin R (⇑f '' ↑s)) :=
+    adjoin_equiv R S T f (s.toSet)-/
+  rw [Algebra.essFiniteType_iff] at *
+  obtain ⟨X, hX⟩ := h
+  use X.map f
+  intro u
+  obtain ⟨s, hs⟩ := hX (f.symm.toAlgHom u)
+  use f s
+  refine ⟨?_, ?_, ?_⟩
+  ·
+    -- rw [Algebra.adjoin_image _ f.toAlgHom X]
+    -- rw [Algebra.adjoin_image, Subalgebra.mem_map]
+
+    sorry
+
+  · rw [MulEquiv.isUnit_map]
+    exact hs.right.left
   ·
     sorry
-  · sorry
+
+example (R S T : Type) [CommRing R] [CommRing S] [CommRing T] [Algebra R S] [Algebra R T]
+    (f : S ≃ₐ[R] T) (M : Submonoid R) (h : IsLocalization M S) :
+  IsLocalization M T := by
+  exact IsLocalization.isLocalization_of_algEquiv M f
+
+@[stacks 030P]
+lemma IntermediateOfInfiniteSeparable_InfiniteSeparable {k K : Type} [Field k] [Field K]
+    [Algebra k K] (K' : IntermediateField k K) (h : InfiniteSeparable k K) :
+    InfiniteSeparable k K' := by
+  intro L hFin
+  let hEquiv : L ≃ₐ[k] (IntermediateField.lift L) := IntermediateField.liftAlgEquiv L
+  have hFin' : Algebra.EssFiniteType k (IntermediateField.lift L) := by
+
+
+    sorry
+  have h' := h (K'.lift L) hFin'
+
+
+  sorry
